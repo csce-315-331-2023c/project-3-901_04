516143cdb670838f57fa1de227ace08ec9eac710
GigaSmurf <45702710+GigaSmurf@users.noreply.github.com>
2023-10-26 14:25:53 -0500

    Accessibility Widget Created

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import Menu from './pages/Menu';
 import Customer from './pages/Customer';
 import Cashier from './pages/Cashier';
 import Manager from './pages/Manager';
+import AccessibilityWidget from './components/AccessibilityWidget';
 
 // Import css for navbar
 import './styles/navbar.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/customer" element={<Customer />} />
-        <Route path="/" exact element={<Home />} />
-        <Route path="/cashier" element={<Cashier />} />
-        <Route path="/manager" element={<Manager />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/cashier" element={<Cashier />} />
+          <Route path="/manager" element={<Manager />} />
+        </Routes>
+      </Router>
+      <AccessibilityWidget />
+    </div>
   );
 }
 
