a2009cc68cab73b589dac0d6cbb08e8ee909899e
GigaSmurf <45702710+GigaSmurf@users.noreply.github.com>
2023-10-31 22:47:58 -0500

    Linked Backend for Menu Page

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Setup CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // this allows requests from your frontend server
+    optionsSuccessStatus: 200
+}));
+
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Mos Irish Pub Backened!');
+});
+
+app.get('/api/menu', async (req, res) => {
+    try {
+        const entreeRes = await pool.query('SELECT entree_name, price FROM entrees WHERE togo = false;');
+        const drinkRes = await pool.query('SELECT drink_name, price FROM drinks');
+
+        const menu = {
+            entrees: entreeRes.rows,
+            drinks: drinkRes.rows
+        };
+
+        res.json(menu);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
 });
 
 app.listen(PORT, () => {
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import '../styles/Menu.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/Menu.css';
 
 function Menu() {
+  const [menu, setMenu] = useState({ entrees: [], drinks: [] });
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/menu') //backend url
+      .then(res => res.json())
+      .then(data => {
+        setMenu(data);
+      });
+  }, []);
+
   return (
     <div className='menu'>
-      <div>Menu</div>
+      <div className="entrees">
+        <h1>Entrees</h1>
+        {menu.entrees.map((entree, index) => (
+          <p key={index}>Entree: {entree.entree_name}, Price: {entree.price}</p>
+        ))}
+      </div>
+
+      <div className="drinks">
+        <h1>Drinks</h1>
+        {menu.drinks.map((drink, index) => (
+          <p key={index}>Drink: {drink.drink_name}, Price: {drink.price}</p>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
