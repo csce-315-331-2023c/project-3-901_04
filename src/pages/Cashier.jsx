86ca5a258a608a233080f8b20f44530f30cdd278
William Yang <jhyang924@gmail.com>
2023-11-05 17:17:28 -0600

    Added pricing for items and the display. Implemented removing single and all ordered items. Rearranged display to have the menu options only taking 2/3 of the page.

--- src/pages/Cashier.jsx
+++ src/pages/Cashier.jsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react';
 import '../styles/Cashier.css';
 import '@mui/material'; 
 import { TextField } from '@mui/material';
+import {Grid} from '@mui/material';
 
 function Cashier() {
   //array of all menu items
   const [array, setArray] = useState([]);
   //array containing order items
   const [order, setOrder] = useState([]);
+  //array containing order prices
+  const [orderPrices, setOrderP] = useState([]);
 
+  //gets the menu contents just once and sets up all menu item buttons and their functions.
   useEffect(() => {
     const temparray = [];
     const getData = async () => {
@@ -19,24 +23,28 @@ function Cashier() {
           : 'http://localhost:3001/api/menu';
         const response = await fetch(backendURL);
         const res = await response.json();
-        const numItems = await (Object.keys(res.entrees).length + Object.keys(res.drinks).length);
-        const numEntrees = await (Object.keys(res.entrees).length);
-        const numDrinks = await (Object.keys(res.drinks).length);
+        const numItems = (Object.keys(res.entrees).length + Object.keys(res.drinks).length);
+        const numEntrees = (Object.keys(res.entrees).length);
+        const numDrinks = (Object.keys(res.drinks).length);
         console.log("length: ", numItems);
         console.log("data success: ", res);
         for (var i = 0; i < numEntrees; i ++) {
           const tempName = res.entrees[i].entree_name;
+          const tempPrice = parseFloat(res.entrees[i].price);
           temparray.push(<button onClick={function itemPress() {
             console.log(tempName);
-            setOrder(array => [...array, tempName + '\n'] );
+            setOrder(order => [...order, tempName] );
+            setOrderP(orderPrices => [...orderPrices, tempPrice]);
           }}>{res.entrees[i].entree_name}</button>);
           console.log("pushed: ", temparray[i]);
         }
         for (var i = 0; i < numDrinks; i ++) {
           const tempName = res.drinks[i].drink_name;
+          const tempPrice = res.drinks[i].price;
           temparray.push(<button onClick={function itemPress() {
             console.log(tempName);
-            setOrder(array => [...array, tempName + '\n'] );
+            setOrder(order => [...order, tempName] );
+            setOrderP(orderPrices => [...orderPrices, tempPrice]);
           }}>{res.drinks[i].drink_name}</button>);
           console.log("pushed: ", temparray[i]);
         }
@@ -54,35 +62,61 @@ function Cashier() {
     <div>
         {(array.length > 0) ? (
             <div> 
+
+              <Grid container spacing={0.5}>
               {/*All menu item buttons*/}
-              {array}
+                <Grid item xs={8}>
+                  <>{array}</>
+                </Grid>
 
-              {/*Textbox for viewing current order TODO*/}
-              <TextField
-                disabled
-                id="outlined-multiline-flexible"
-                multiline
-                //defaultvalue="Ordered items will show up here"
-                value={order}
-                label="Order"
-                helperText="Total: "
-                onChange={order}
-              />
+                {/*Textbox for viewing current order TODO need to mess with disabled state*/}
+                <Grid item xs container spacing = {0}>
+                  {/*The names of ordered items*/}
+                  <TextField
+                    fullwidth
+                    disabled
+                    id="outlined-multiline-flexible"
+                    multiline
+                    //defaultvalue="Ordered items will show up here"
+                    value={order.join("\n")}
+                    label="Order"
+                    onChange={order}
+                    //maxRows = {order.length}
+                  />
 
-              <button onClick={function removeOne() {
-                  console.log('Undo called');
-                  setOrder(array => [...array.slice(0,-1)] );
-              }}>Remove Last Item</button>
+                  {/*The prices of ordered items*/}
+                  <TextField
+                    fullwidth
+                    disabled
+                    id="outlined-multiline-flexible"
+                    multiline
+                    //defaultvalue="Ordered items will show up here"
+                    value={orderPrices.join("\n")}
+                    label="Price"
+                    helperText={"Total: " + orderPrices.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}
+                    onChange={orderPrices}
+                  />
 
-              <button onClick={function clearAll() {
-                  console.log('Clear all called');
-                  setOrder([]);
-              }}>Clear All</button>
+                  <Grid item xs>
+                    <button onClick={function removeOne() {
+                        console.log('Remove one called');
+                        setOrder(order => [...order.slice(0,-1)] );
+                        setOrderP(orderPrices => [...orderPrices.slice(0,-1)] );
+                    }}>Remove Last Item</button>
 
-              {/*TODO*/}
-              <button onClick={function submit() {
-                  console.log('Place order called');
-              }}>Place Order</button>
+                    <button onClick={function clearAll() {
+                        console.log('Clear all called');
+                        setOrder([]);
+                        setOrderP([]);
+                    }}>Clear All</button>
+
+                    {/*TODO need to implement SQL*/}
+                    <button onClick={function submit() {
+                        console.log('Place order called');
+                    }}>Place Order</button>
+                  </Grid>
+                </Grid>
+              </Grid>
             </div>
         ): (
             <h>loading...</h>
@@ -90,6 +124,5 @@ function Cashier() {
     </div>
 )
 }
-
 
 export default Cashier
