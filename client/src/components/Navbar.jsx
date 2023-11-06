fb1adabc3e4ee108745343ddc43b2bb93eb2dcdf
GigaSmurf <45702710+GigaSmurf@users.noreply.github.com>
2023-11-06 14:10:11 -0600

    main rebase

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logoimg from '../media/mos.svg';
+
 const Navbar = () => {
     return (
         <nav>
             <Link to="/menu">Menu</Link>
             <Link to="/customer">Customer</Link>
-            <Link to="/">Home</Link>
+            <Link to="/" Home>
+                <img src={logoimg} height="40" width="100" />
+            </Link>
             <Link to="/cashier">Cashier</Link>
             <Link to="/manager">Manager</Link>
         </nav>
