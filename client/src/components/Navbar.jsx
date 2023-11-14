2f6f878defdb31fbaf516450938e86a804a893ae
GigaSmurf <45702710+GigaSmurf@users.noreply.github.com>
2023-11-14 09:31:34 -0600

    Cookies consent and Navbar based on employee

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoimg from '../media/mos.svg';
 
-
 const Navbar = () => {
     const [visible, setVisible] = useState(true);
+    const isEmployee = JSON.parse(localStorage.getItem('isEmployee')); // Retrieve the isEmployee flag
+
+    console.log(isEmployee);
 
     const handleScroll = () => {
         const currentScrollPos = window.scrollY;
@@ -22,13 +24,24 @@ const Navbar = () => {
         <>
             <div className="hover-reveal-area" onMouseOver={() => setVisible(true)}></div>
             <nav className={visible ? 'navbar visible' : 'navbar hidden'}>
-                <Link to="/menu">Menu</Link>
-                <Link to="/customer">Customer</Link>
-                <Link to="/home" className="home-link">
-                    <img src={logoimg} alt="logo" height="40" width="100" />
-                </Link>
-                <Link to="/cashier">Cashier</Link>
-                <Link to="/manager">Manager</Link>
+                {
+                    !isEmployee && <>
+                        <Link to="/menu">Menu</Link>
+                        <Link to="/home" className="home-link">
+                            <img src={logoimg} alt="logo" height="40" width="100" />
+                        </Link>
+                        <Link to="/customer">Customer</Link>
+                    </>
+                }
+                {isEmployee && <>
+                    <Link to="/menu">Menu</Link>
+                    <Link to="/customer">Customer</Link>
+                    <Link to="/home" className="home-link">
+                        <img src={logoimg} alt="logo" height="40" width="100" />
+                    </Link>
+                    <Link to="/cashier">Cashier</Link>
+                    <Link to="/manager">Manager</Link>
+                </>}
             </nav>
         </>
     );
