3d1a9500e7790a874ad3c8d321571dbf508aa0b8
Jack S <ssjacksmith23@gmail.com>
2023-11-14 23:21:13 -0600

    Maps working, needs cleanup

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,14 @@
+// Map.jsx
+import React from 'react';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const Map = withGoogleMap(({ location }) => (
+  <GoogleMap
+    defaultZoom={15}
+    defaultCenter={{ lat: location.lat, lng: location.lng }}
+  >
+    <Marker position={{ lat: location.lat, lng: location.lng }} />
+  </GoogleMap>
+));
+
+export default Map;
