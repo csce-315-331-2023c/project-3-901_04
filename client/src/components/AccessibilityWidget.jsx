651e4b28ff9e227b2b1503ec4bb66c09ad971e3a
McCreeFromGameCube <123025418+McCreeFromGameCube@users.noreply.github.com>
2023-11-14 23:48:30 -0600

    Added translate API (?)

--- client/src/components/AccessibilityWidget.jsx
+++ client/src/components/AccessibilityWidget.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/AccessibilityWidget.css'
+/* global window */
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles/AccessibilityWidget.css';
 
 function AccessibilityWidget({ isDarkMode, setIsDarkMode }) {
     const [isOpen, setIsOpen] = useState(false);
-
 
     const handleWidgetClick = () => {
         setIsOpen(true);
@@ -13,8 +13,33 @@ function AccessibilityWidget({ isDarkMode, setIsDarkMode }) {
         setIsOpen(false);
     };
 
+    const weglotContainerRef = useRef(null);
+
+  useEffect(() => {
+    // Load Weglot script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://cdn.weglot.com/weglot.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+        if (weglotContainerRef.current) {
+          window.Weglot.initialize({
+            api_key: 'wg_12137e766e1482a9e70dd617bdb421029',
+            targetLang: 'en', // Set the target language directly
+          });
+        }
+      };
+
+    // Cleanup: remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
     return (
         <div className>
+            <div ref={weglotContainerRef}>
             <div className="accessibility-widget" onClick={handleWidgetClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" style={{ fill: 'white' }} viewBox="0 0 24 24" width="30px" height="30px">
                     <path d="M0 0h24v24H0V0z" fill="none"></path>
@@ -32,10 +57,12 @@ function AccessibilityWidget({ isDarkMode, setIsDarkMode }) {
                         <button onClick={() => setIsDarkMode(!isDarkMode)}>
                             {isDarkMode ? "Light Mode" : "Dark Mode"}
                         </button>
+                        
                         {/* Add more buttons or switches as needed */}
                     </div>
                 </div>
             )}
+            </div>
         </div>
     );
 }
