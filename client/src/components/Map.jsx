30b6d12285f8846ac697ae2d8b372ab6f933c8de
Jack S <ssjacksmith23@gmail.com>
2023-11-17 09:26:18 -0600

    Map functional

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,14 +1,68 @@
 // Map.jsx
-import React from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import React, {useEffect} from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';  
 
-const Map = withGoogleMap(({ location }) => (
+/* const Map = withGoogleMap(({ location }) => (
   <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat: location.lat, lng: location.lng }}
   >
     <Marker position={{ lat: location.lat, lng: location.lng }} />
   </GoogleMap>
-));
+)); */
 
-export default Map;
+/* const Map = () => {
+  useEffect(() => {
+    // Load Google Maps API script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBngrehX3C19VORW1xnYpYn8GyYbIsD-GA&libraries=maps,marker&v=beta';
+    script.async = true;
+
+    script.onerror = () => {
+      console.error('Error loading Google Maps API script.');
+    };
+
+    document.head.appendChild(script);
+
+    // Cleanup the script tag on component unmount
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div style={{ height: '100%' }}>
+      <gmp-map center="30.623876571655273,-96.33992004394531" zoom="14" map-id="DEMO_MAP_ID">
+        <gmp-advanced-marker position="30.623876571655273,-96.33992004394531" title="My location"></gmp-advanced-marker>
+      </gmp-map>
+    </div>
+  );
+};
+
+export default Map; */
+class GMap extends React.Component {
+  render() {
+      const mapStyles = {
+          width: 700,
+          height: 500,
+      };
+
+      return (
+          <Map
+              google={this.props.google}
+              zoom={16}
+              style={mapStyles}
+              initialCenter={{ lat: 30.623876571655273, lng: -96.33992004394531 }}
+          >
+              <Marker
+                  position={{ lat: 30.623876571655273, lng: -96.33992004394531 }}
+                  title={"Mo's Irish Pub"}
+              />
+          </Map>
+      );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBngrehX3C19VORW1xnYpYn8GyYbIsD-GA'
+})(GMap);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import '../styles/Home.css'
 import Weather from '../components/weather';
-import Map from '../components/Map';
+import GMap from '../components/Map';
 
 function Home() {
   const user = JSON.parse(localStorage.getItem('user')); // Retrieve the user data
-  const mapLocation = { lat: 30.623977661132812, lng: -96.33989715576172 };
 
   return (
       <div>
           <h1>Welcome, {user?.name}!</h1> {/* Display the name */}
           <Weather/>
-          <Map
-            containerElement={<div style={{ height: '400px' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-            location={mapLocation}
-          />
+          <GMap/>
       </div>
   );
 }
