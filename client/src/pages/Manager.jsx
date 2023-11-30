6cb18c4b54c15ebb6af4256f94ef683613a4e98e
GigaSmurf <45702710+GigaSmurf@users.noreply.github.com>
2023-11-30 01:45:42 -0600

    Add Inventory Functionality

--- client/src/pages/Manager.jsx
+++ client/src/pages/Manager.jsx
@@ -1,12 +1,371 @@
-import React from 'react'
-import '../styles/Manager.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/Manager.css';
+import Button from '@mui/material/Button';
+import { TextField, Grid } from '@mui/material';
+import axios from 'axios';
+import Greeting from '../components/Greeting';
+import { useForm } from 'react-hook-form';
 
 function Manager() {
+
+  const [DBItems, setDBItems] = useState({ menuItems: [], inventoryItems: [] });
+  const [activeView, setActiveView] = useState(DBItems.menuItems);
+
+  const [menuItemRecipe, setRecipe] = useState([]);
+  const [displayedRecipe, setDisplayedRecipe] = useState([]);
+
+  const [activeGrid, setActiveGrid] = useState(2);
+
+  const [menuItemName, setMenuItemName] = useState('Select a Menu Item');
+  const [displayedMenuItemName, setDisplayedMenuItemName] = useState();
+
+  const [menuItemPrice, setMenuItemPrice] = useState('Select a Menu Item');
+  const [displayedMenuItemPrice, setDisplayedMenuItemPrice] = useState();
+
+  const [menuItemTogo, setMenuitemTogo] = useState('Select a Menu Item');
+  const [displayedMenuItemTogo, setDisplayedMenuitemTogo] = useState();
+
+
+  const [invItemName, setInvItemName] = useState('Select an Inventory Item');
+  const [displayedInvItemName, setDisplayedInvItemName] = useState();
+
+  const [invItemStock, setInvItemStock] = useState('Select an Inventory Item');
+  const [displayedInvItemStock, setDisplayedInvItemStock] = useState();
+
+  const [invItemCost, setInvItemCost] = useState('Select an Inventory Item');
+  const [displayedInvItemCost, setDisplayedInvItemCost] = useState();
+
+  const [invItemMinimum, setinvItemMinimum] = useState('Select an Inventory Item');
+  const [displayedInvItemMinimum, setDisplayedinvItemMinimum] = useState();
+
+
+  const [showingMenu, setShowingMenu] = useState(true);
+
+  useEffect(() => {
+    const backendURL = process.env.NODE_ENV === 'production'
+      ? 'https://mos-irish-server-901-04.vercel.app/api/managerMenu'
+      : 'http://localhost:3001/api/managerMenu';
+    fetch(backendURL) //backend url
+      .then(res => res.json())
+      .then(data => {
+        setDBItems(data);
+        setActiveView(DBItems.menuItems);
+        console.log(data);
+      });
+    handleViewToggle();
+  }, []);
+
+  useEffect(() => {
+    console.log('Updated menuItemRecipe:', menuItemRecipe);
+
+    const updatedDisplay = menuItemRecipe.map((item) => ({
+      name: item.item_name,
+      quantity: item.quantity,
+    }));
+
+    setDisplayedRecipe(updatedDisplay);
+
+  }, [menuItemRecipe]);
+
+  useEffect(() => {
+    console.log("Updated menuItemName: ", menuItemName);
+    console.log("Updated menuItemPrice: ", menuItemPrice);
+    console.log("Updated menuItemTogo: ", menuItemTogo);
+    console.log("Updated invItemName: ", invItemName);
+    console.log("Updated invItemStock: ", invItemStock);
+    console.log("Updated invItemCost: ", invItemCost);
+    console.log("Updated invItemMinimum: ", invItemMinimum);
+
+    setDisplayedMenuItemName(menuItemName);
+    setDisplayedMenuItemPrice(menuItemPrice);
+    if (menuItemTogo) {
+      setDisplayedMenuitemTogo('☑');
+    }
+    else {
+      setDisplayedMenuitemTogo('☒');
+    }
+    setDisplayedInvItemName(invItemName);
+    setDisplayedInvItemStock(invItemStock);
+    setDisplayedInvItemCost(invItemCost);
+    setDisplayedinvItemMinimum(invItemMinimum);
+
+  }, [menuItemName, menuItemPrice, menuItemTogo, invItemName, invItemStock, invItemCost, invItemMinimum]);
+
+  function handleItemButtonClick(buttonText) {
+    console.log('Button clicked: ', buttonText);
+    const backendURL2 = process.env.NODE_ENV === 'production'
+      ? 'https://mos-irish-server-901-04.vercel.app/api/recipe'
+      : 'http://localhost:3001/api/recipe';
+    const fetchRecipeData = async () => {
+      try {
+        const itemQuery = await axios.get(backendURL2, {
+          params: {
+            requestedItem: buttonText,
+          },
+        });
+
+        setRecipe(itemQuery.data.recipe);
+        setMenuItemName(itemQuery.data.menuItemInfo[0].item_name);
+        setMenuItemPrice(itemQuery.data.menuItemInfo[0].price);
+        setMenuitemTogo(itemQuery.data.menuItemInfo[0].togo);
+        setInvItemName(itemQuery.data.invItemInfo[0].item_name);
+        setInvItemStock(itemQuery.data.invItemInfo[0].stock);
+        setInvItemCost(itemQuery.data.invItemInfo[0].cost);
+        setinvItemMinimum(itemQuery.data.invItemInfo[0].min_stock_warning);
+
+      } catch (error) {
+        console.error('Recipe error', error);
+      }
+    };
+    fetchRecipeData();
+  }
+
+  const inventoryDataDisplay = () => {
+    return (
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <h3>Name: <div className="displayedValue">{displayedInvItemName}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New Name"
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={6}>
+          <h3>Stock: <div className="displayedValue">{displayedInvItemStock}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New Stock"
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={6}>
+          <h3>Cost: <div className="displayedValue">${displayedInvItemCost}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New Cost"
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={6}>
+          <h3>Minimum Stock Warning: <div className="displayedValue">{displayedInvItemMinimum}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New MSW"
+          >
+          </TextField>
+        </Grid>
+      </Grid>
+    );
+  };
+
+  const menuDataDisplay = () => {
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={6} className="editPaneItem">
+          <h3>Name: <div className="displayedValue">{displayedMenuItemName}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New Name"
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={6} className="editPaneItem">
+          <h3>Price: <div className="displayedValue">${displayedMenuItemPrice}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="Input New Price"
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={6} className="editPaneItem">
+          <h3>To-Go: <div className="displayedValue">{displayedMenuItemTogo}</div></h3>
+          <TextField
+            variant="outlined"
+            //value={text}
+            //onChange={handleInputChange}
+            placeholder="True/False"
+          >
+          </TextField>
+        </Grid>
+      </Grid>
+    );
+  };
+
+  const handleViewToggle = () => {
+    setActiveGrid(activeGrid === 1 ? 2 : 1);
+    setActiveView(activeView === DBItems.menuItems ? DBItems.inventoryItems : DBItems.menuItems);
+    setShowingMenu(!showingMenu);
+    setRecipe([{ item_name: 'Select a menu item to update', quantity: 0 }]);
+    setMenuItemName('Select a Menu Item');
+    setMenuItemPrice('Select a Menu Item');
+    setMenuitemTogo('Select a Menu Item');
+    setInvItemName('Select an Inventory Item');
+    setInvItemCost('Select an Inventory Item');
+    setInvItemStock('Select an Inventory Item');
+    setinvItemMinimum('Select an Inventory Item');
+    setShowAddItemForm(false);
+  };
+
+  const dataGridContainer = () => {
+    return (
+      <div>
+        {activeGrid === 1 && inventoryDataDisplay()}
+        {activeGrid === 2 && menuDataDisplay()}
+      </div>
+    );
+  };
+
+  const ManagerNavButtons = ({ items }) => {
+    return (
+      <div className="scrollView" style={{ height: '600px', overflowY: 'scroll' }}>
+        <div>
+          {items.map((item) => (
+            <Button onClick={() => handleItemButtonClick(item.item_name)} variant="outlined" className="managerMenuButton" key={item}>
+              {item.item_name}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+    );
+  };
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmitNewItem = async (data) => {
+    try {
+      const itemData = {
+        itemName: data.itemName,
+        stock: data.stock,
+        cost: data.cost,
+        minStockWarning: data.minStockWarning
+      };
+
+      const backendURL = process.env.NODE_ENV === 'production'
+        ? 'https://mos-irish-server-901-04.vercel.app/api/addInventoryItem'
+        : 'http://localhost:3001/api/addInventoryItem';
+
+
+
+      await axios.post(backendURL, itemData);
+      // console.log('Response:', response.data);
+      alert('Item added successfully!');
+
+      reset(); // Reset form fields after submission
+    } catch (error) {
+      console.error('Failed to add item', error);
+      alert('Failed to add item.');
+    }
+  };
+
+  const newItemForm = () => {
+    return (
+      <div className="managerForm">
+        <h2>Add New Inventory Item</h2>
+        <form onSubmit={handleSubmit(onSubmitNewItem)}>
+          <div className="formField">
+            <TextField {...register('itemName')} placeholder="Item Name" required fullWidth />
+          </div>
+          <div className="formField">
+            <TextField {...register('stock')} placeholder="Stock" type="number" required fullWidth />
+          </div>
+          <div className="formField">
+            <TextField {...register('cost')} placeholder="Cost" type="number" required fullWidth />
+          </div>
+          <div className="formField">
+            <TextField {...register('minStockWarning')} placeholder="Minimum Stock Warning" type="number" required fullWidth />
+          </div>
+          <Button type="submit" className="submitButton" variant="contained">Add Item</Button>
+        </form>
+      </div>
+    );
+  };
+
+
+  const [showAddItemForm, setShowAddItemForm] = useState(false);
+
+  // Function to toggle the add item form
+  const handleToggleAddItemForm = () => {
+    setShowAddItemForm(!showAddItemForm);
+    if (!showAddItemForm) {
+      // Add any state resets here if you want to hide other components when showing the form
+      // For example:
+      setShowingMenu(false);
+    }
+  };
+
+
+
+  console.log("showingMenu: ", showingMenu);
   return (
     <div className='manager'>
-      <div>Manager</div>
+      <Grid container spacing={2}>
+        <Grid item xs={3.6}>
+          <div className='leftGrid'>
+            <div className='scrollView'>
+              <div>
+                <Button className="managerMenuSwapButton" variant="contained" disableElevation onClick={handleViewToggle}>
+                  Swap Menu/Inventory
+                </Button>
+                <Button className="toggleAddItemFormButton" variant="contained" disableElevation onClick={handleToggleAddItemForm}>
+                  {showAddItemForm ? 'Hide Add Item Form' : 'Show Add Item Form'}
+                </Button>
+              </div>
+              {/* Only show the navigation buttons if the addItemForm is not shown */}
+              {!showAddItemForm && <ManagerNavButtons items={activeView} />}
+            </div>
+          </div>
+        </Grid>
+
+        {showAddItemForm ? (
+          // Show the addItemForm when showAddItemForm is true
+          <Grid item xs={5.4}>
+            {newItemForm()}
+          </Grid>
+        ) : (
+          // Otherwise, show menu or inventory display based on showingMenu
+          <Grid item xs={5.4}>
+            {showingMenu ? menuDataDisplay() : inventoryDataDisplay()}
+          </Grid>
+        )}
+
+        {/* Recipe panel is always shown on the right-hand side */}
+        <Grid item xs={3}>
+          <div className='rightGrid'>
+            <div>
+              <div>
+                <h1>Recipe</h1>
+              </div>
+              <div className="scrollView" style={{ height: '600px', overflowY: 'scroll' }}>
+                <div>
+                  {displayedRecipe && displayedRecipe.map((item, index) => (
+                    <p key={index}>
+                      {item.name}: <b>{item.quantity}</b>
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
+
 
 export default Manager
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const User = require('./mongo');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(cors()); //enable cors for all routes
 app.use(express.json());
 
@@ -32,12 +33,94 @@ app.get('/api/menu', async (req, res) => {
             drinks: drinkRes.rows
         };
 
+        console.log("menu Query successful. Sending json.");
+
+        res.json(menu);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Query Failure :(');
+    }
+});
+
+app.get('/api/managerMenu', async (req, res) => {
+    try {
+        const menuRes = await pool.query('(SELECT entree_name AS "item_name" FROM entrees UNION SELECT drink_name FROM drinks) ORDER BY item_name;');
+        const inventoryRes = await pool.query('SELECT item_name FROM inventory ORDER BY item_name;');
+
+        const managerMenu = {
+            menuItems: menuRes.rows,
+            inventoryItems: inventoryRes.rows
+        };
+
+        console.log("managerMenu Query successful. Sending json.");
+        res.json(managerMenu);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Query Failure :(');
+    }
+});
+
+app.get('/api/menu', async (req, res) => {
+    try {
+        const entreeRes = await pool.query('SELECT entree_name, price FROM entrees WHERE togo = false;');
+        const drinkRes = await pool.query('SELECT drink_name, price FROM drinks');
+
+        const menu = {
+            entrees: entreeRes.rows,
+            drinks: drinkRes.rows
+        };
+
         console.log("Query successful. Sending json.");
 
         res.json(menu);
     } catch (err) {
         console.error(err);
         res.status(500).send('Query Failure :(');
+    }
+});
+
+
+app.get('/api/recipe', async (req, res) => {
+
+    const reqItem = req.query.requestedItem;
+    console.log("Finding recipe for item: ", reqItem);
+
+    try {
+        const recipeResult = await pool.query("SELECT item_name, quantity FROM (SELECT DISTINCT entree_name, item_name, quantity FROM entreerecipes JOIN entrees ON entrees.id = entreerecipes.entree_id JOIN inventory ON inventory.id = entreerecipes.inventory_id WHERE entree_name = '" + reqItem + "' UNION SELECT DISTINCT drink_name, item_name, quantity FROM drinkrecipes JOIN drinks ON drinks.id = drinkrecipes.drink_id JOIN inventory ON inventory.id = drinkrecipes.inventory_id WHERE drink_name = '" + reqItem + "') AS recipes;");
+        const menuResult = await pool.query("SELECT entree_name AS item_name, price, togo FROM entrees WHERE entree_name = '" + reqItem + "' UNION SELECT drink_name AS item_name, price, togo FROM drinks WHERE drink_name = '" + reqItem + "';");
+        const invResult = await pool.query("SELECT item_name, stock, cost, min_stock_warning FROM inventory WHERE item_name = '" + reqItem + "';");
+        console.log('/api/recipe queries success');
+        if (menuResult.rows.length != 0 && invResult.rows.length == 0) {//If reqItem is a menu item
+            res.json({
+                recipe: recipeResult.rows,
+                menuItemInfo: menuResult.rows,
+                invItemInfo: [{ item_name: '', stock: -1, cost: -1, min_stock_warning: -1 }]
+            });
+        }
+        else if (menuResult.rows.length == 0 && invResult.rows.length != 0) { //If reqItem is an inventory item
+            res.json({
+                recipe: recipeResult.rows,
+                menuItemInfo: [{ item_name: '', price: -1, togo: false }],
+                invItemInfo: invResult.rows
+            })
+        }
+        else {
+            res.json({
+                recipe: recipeResult.rows,
+                menuItemInfo: menuResult.rows,
+                invItemInfo: invResult.rows
+            })
+        }
+
+        console.log("Recipe:", recipeResult.rows);
+        console.log("menuResult:", menuResult.rows);
+        console.log("invResult: ", invResult.rows);
+        return;
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error executing the query' });
+        return;
     }
 });
 
@@ -102,7 +185,7 @@ app.post('/postid', async (req, res) => {
     //Gets the ids of items
     try {
         //Arbitrary amount; surely no one orders 100 items in a single sitting right?
-        while (count < 100){
+        while (count < 100) {
             if (orders[count] == null) {
                 break;
             }
@@ -139,11 +222,11 @@ app.post('/postid', async (req, res) => {
     //Insert into orderentreecontents
     var orderEntreeString = "INSERT INTO orderentreecontents (order_id, entree_id) VALUES";
     try {
-        for (var i = 0; i < entreearr.length; i ++) {
+        for (var i = 0; i < entreearr.length; i++) {
             if (i != 0) {
                 orderEntreeString = orderEntreeString + ",";
             }
-            orderEntreeString = orderEntreeString + " (" + parseInt(parseInt(highestOrderId.rows[0].id) + 1) + ", " + entreearr[i] +")";
+            orderEntreeString = orderEntreeString + " (" + parseInt(parseInt(highestOrderId.rows[0].id) + 1) + ", " + entreearr[i] + ")";
         }
         orderEntreeString = orderEntreeString + ";";
         pool.query(orderEntreeString);
@@ -155,11 +238,11 @@ app.post('/postid', async (req, res) => {
     //Insert into orderdrinkcontents
     var orderDrinkString = "INSERT INTO orderdrinkcontents (order_id, drink_id) VALUES";
     try {
-        for (var i = 0; i < drinkarr.length; i ++) {
+        for (var i = 0; i < drinkarr.length; i++) {
             if (i != 0) {
                 orderDrinkString = orderDrinkString + ",";
             }
-            orderDrinkString = orderDrinkString + " (" + parseInt(parseInt(highestOrderId.rows[0].id) + 1) + ", " + drinkarr[i] +")";
+            orderDrinkString = orderDrinkString + " (" + parseInt(parseInt(highestOrderId.rows[0].id) + 1) + ", " + drinkarr[i] + ")";
         }
         orderDrinkString = orderDrinkString + ";";
         pool.query(orderDrinkString);
@@ -173,7 +256,7 @@ app.post('/postid', async (req, res) => {
     var getDrinkInventory = "SELECT DISTINCT drink_name, item_name, quantity FROM drinkrecipes JOIN drinks ON drinks.id = drinkrecipes.drink_id JOIN inventory ON inventory.id = drinkrecipes.inventory_id WHERE drink_id in (";
     var allInventory;
     try {
-        for (var i = 0; i < entreearr.length; i ++) {
+        for (var i = 0; i < entreearr.length; i++) {
             if (i != 0) {
                 getEntreeInventory = getEntreeInventory + ",";
             }
@@ -183,7 +266,7 @@ app.post('/postid', async (req, res) => {
         const entreeInventoryQuantities = await pool.query(getEntreeInventory);
         //res.send(JSON.stringify((entreeInventoryQuantities).rows[0].quantity));
 
-        for (var i = 0; i < drinkarr.length; i ++) {
+        for (var i = 0; i < drinkarr.length; i++) {
             if (i != 0) {
                 getDrinkInventory = getDrinkInventory + ",";
             }
@@ -220,7 +303,7 @@ function getTimeDate() {
     const hour = getHour();
     const minute = getMinute();
     const second = getSecond();
-    return(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
+    return (year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
 }
 
 function getYear() {
@@ -233,14 +316,14 @@ function getMonth() {
     return time.getMonth() + 1;
 }
 
-Date.prototype.getWeek = function() {
+Date.prototype.getWeek = function () {
     var start = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - start) / 86400000) + start.getDay() + 1) / 7);
 }
 
 function getDay() {
     const time = new Date();
-    return time.toLocaleString("en-US", { day : '2-digit'})
+    return time.toLocaleString("en-US", { day: '2-digit' })
 }
 
 function getHour() {
@@ -257,8 +340,27 @@ function getSecond() {
     const time = new Date();
     return time.getSeconds();
 }
+app.post('/api/addInventoryItem', async (req, res) => {
+    const { itemName, stock, cost, minStockWarning } = req.body;
+
+    try {
+        const maxIdResult = await pool.query('SELECT MAX(id) FROM inventory');
+        const idMax = maxIdResult.rows[0].max + 1;
+
+        const queryText = 'INSERT INTO Inventory (id, item_name, stock, cost, min_stock_warning) VALUES ($1, $2, $3, $4, $5)';
+        const values = [idMax, itemName, stock, cost, minStockWarning];
+        await pool.query(queryText, values);
+
+        res.json({ message: 'Item added successfully', itemId: idMax });
+    } catch (error) {
+        console.error('Error adding item to inventory', error);
+        res.status(500).json({ error: 'Error adding item to inventory', message: error.message });
+    }
+});
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
